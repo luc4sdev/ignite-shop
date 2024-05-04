3d05b5dab4a45929688b555b0ae067bf2e6c5aca
Lucas Pereira França <lucas.pfranca01@gmail.com>
2024-05-04 11:28:13 -0300

    feat: add .env.example file

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -59,7 +59,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
    const session = await stripe.checkout.sessions.retrieve(sessionId, {
       expand: ['line_items', 'line_items.data.price.product']
    })
-
+   console.log(session)
    const customerName = session.customer_details?.name
    const product = session.line_items?.data[0].price?.product as Stripe.Product
    return {
